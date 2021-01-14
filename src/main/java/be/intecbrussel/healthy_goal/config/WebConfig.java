67beb6c5b3b6915a8e4019d7f0be414828f63552
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-01-14 21:51:52 +0100

    testing bootstrap

--- src/main/java/be/intecbrussel/healthy_goal/config/WebConfig.java
+++ src/main/java/be/intecbrussel/healthy_goal/config/WebConfig.java
@@ -4,11 +4,10 @@ package be.intecbrussel.healthy_goal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig {
 
     @Bean
     public ViewResolver viewResolver() {
